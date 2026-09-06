4686d68a5639fb7eb277e011bdcea71d252a16f9
kwas-sudo <adilmast03@gmail.com>
2026-09-06 16:31:19 +0500

    Update script.js

--- script.js
+++ script.js
@@ -24,6 +24,58 @@ document.addEventListener("DOMContentLoaded", function () {
             wordCount.textContent = trimmedText.split(/\s+/).length;
         }
 
-    });
+    }); // CASE CONVERTER
+
+function getCaseInput() {
+    return document.getElementById("caseInput");
+}
+
+function convertUpper() {
+    const input = getCaseInput();
+
+    if (input) {
+        input.value = input.value.toUpperCase();
+    }
+}
+
+function convertLower() {
+    const input = getCaseInput();
+
+    if (input) {
+        input.value = input.value.toLowerCase();
+    }
+}
+
+function convertTitle() {
+    const input = getCaseInput();
+
+    if (input) {
+        input.value = input.value
+            .toLowerCase()
+            .replace(/\b\w/g, function(letter) {
+                return letter.toUpperCase();
+            });
+    }
+}
+
+function convertSentence() {
+    const input = getCaseInput();
+
+    if (input) {
+        input.value = input.value
+            .toLowerCase()
+            .replace(/(^\s*\w|[.!?]\s*\w)/g, function(letter) {
+                return letter.toUpperCase();
+            });
+    }
+}
+
+function clearCaseText() {
+    const input = getCaseInput();
+
+    if (input) {
+        input.value = "";
+    }
+}
 
 });
